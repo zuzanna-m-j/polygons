ddd84ac717519cbd2034f5acaec6722c0a7a6c14
Zuzanna Jedlinska <zuzannaj@sas.upenn.edu>
2021-07-01 14:34:03 -0400

    Added overlap check functionality

--- structures.py
+++ structures.py
@@ -1,6 +1,87 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+def onSegment(p, q, r):
+    if ((q[0] <= max(p[0], r[0])) and (q[0] >= min(p[0], r[0])) and
+            (q[1] <= max(p[1], r[1])) and (q[1] >= min(p[1], r[1]))):
+        return True
+    return False
+
+def orientation(p, q, r):
+    # to find the orientation of an ordered triplet (p,q,r)
+    # function returns the following values:
+    # 0 : Colinear points
+    # 1 : Clockwise points
+    # 2 : Counterclockwise
+
+    # See https://www.geeksforgeeks.org/orientation-3-ordered-points/amp/
+    # for details of below formula.
+
+    val = (float(q[1] - p[1]) * (r[0] - q[0])) - (float(q[0] - p[0]) * (r[1] - q[1]))
+    if (val > 0):
+
+        # Clockwise orientation
+        return 1
+    elif (val < 0):
+
+        # Counterclockwise orientation
+        return 2
+    else:
+
+        # Colinear orientation
+        return 0
+
+
+# The main function that returns true if
+# the line segment 'p1q1' and 'p2q2' intersect.
+def Intersection(p1, q1, p2, q2):
+    # Find the 4 orientations required for
+    # the general and special cases
+    o1 = orientation(p1, q1, p2)
+    o2 = orientation(p1, q1, q2)
+    o3 = orientation(p2, q2, p1)
+    o4 = orientation(p2, q2, q1)
+
+    # General case
+    if ((o1 != o2) and (o3 != o4)):
+        return True
+
+    # Special Cases
+
+    # p1 , q1 and p2 are colinear and p2 lies on segment p1q1
+    if ((o1 == 0) and onSegment(p1, p2, q1)):
+        return True
+
+    # p1 , q1 and q2 are colinear and q2 lies on segment p1q1
+    if ((o2 == 0) and onSegment(p1, q2, q1)):
+        return True
+
+    # p2 , q2 and p1 are colinear and p1 lies on segment p2q2
+    if ((o3 == 0) and onSegment(p2, p1, q2)):
+        return True
+
+    # p2 , q2 and q1 are colinear and q1 lies on segment p2q2
+    if ((o4 == 0) and onSegment(p2, q1, q2)):
+        return True
+
+    # If none of the cases
+    return False
+
+
+def OverlapFound(obj1,obj2):
+
+    #FLAG = False -- no overlap found
+    FLAG = False
+    for m in obj1.endpoints:
+        if FLAG == True:
+            break
+        else:
+            for n in obj2.endpoints:
+                FLAG = Intersection(m[0],m[1],n[0],n[1])
+                if FLAG == True:
+                    break
+    return FLAG
+
 
 class Settings():
 
@@ -77,15 +158,15 @@ class Triangle(Shapes):
         s = 0.5 * (s1 + s2 +s3)
         return np.sqrt(s * (s - s1) * (s - s2) * (s - s3))
 
-    def Vectors(self):
+    def EndPoints(self):
 
         v1 = self.v1
         v2 = self.v2
         v3 = self.v3
 
-        a = self.Vector(v1,v2)
-        b = self.Vector(v2,v3)
-        c = self.Vector(v3,v1)
+        a = (v1,v2)
+        b = (v2,v3)
+        c = (v3,v1)
 
         return a,b,c
 
@@ -132,20 +213,21 @@ class Triangle(Shapes):
 
         self.a, self.b, self.c = self.Sides()
         self.x, self.y, self.r = self.Circumcircle()
-        self.vectors = self.Vectors()
+        self.endpoints = self.EndPoints()
 
         #Increase total counters
         self.area = self.Area()
         self.Update_Area()
 
-t1 = Triangle([(0,0),(2,0),(0,3)])
 
-plt.plot(0,0,'o')
-plt.plot(2,0,'o')
-plt.plot(0,3,'o')
-plt.plot(t1.x,t1.y,'o',label = "center")
-plt.legend()
+#test code
+
+t1 = Triangle([(-1,0),(2,0),(0,3)])
+t2 = Triangle([(0,0),(4,0),(2,3)])
+
+for p in t1.vertices:
+    plt.plot(p[0],p[1],'bo--')
+for p in t2.vertices:
+    plt.plot(p[0],p[1],'ro--')
 plt.show()
-print(t1.r)
-r0 = np.sqrt(3**2 + 2**2)/2
-print(r0)
+print(OverlapFound(t1,t2))
