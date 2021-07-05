632af58ee6db40726b992df0c1bdf89d4f3bece5
Zuzanna Jedlinska <zuzannaj@sas.upenn.edu>
2021-07-05 16:38:38 -0400

    base.py created

--- base.py
+++ base.py
@@ -4,6 +4,8 @@
 # Description:
 
 # Imports
+import copy
+
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -18,14 +20,37 @@ import matplotlib.pyplot as plt
 
 class Structure():
 
+    def VLEN(self,x1,y1,x2,y2):
+        return np.sqrt((x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1))
+
+    def TRIANGLE_AREA(self,type):
+
+        v_list = self.vertices[type]
+        AB = self.VLEN(v_list[:4])
+        BC = self.VLEN(v_list[4:8])
+        CA = self.VLEN(v_list[8:])
+
+        # calculate the area using Heron's formula
+        s = 0.5 * (s1 + s2 +s3)
+        return np.sqrt(s * (s - AB) * (s - BC) * (s - CA))
+
+    def QUAD_AREA(self,type):
+
+        v_list = self.vertices[type]
+        AB = self.VLEN(v_list[:4])
+        BC = self.VLEN(v_list[4:8])
+        CD = self.VLEN(v_list[8:12])
+        DA = self.VLEN(v_list[12:])
+
+
     def AREA(self,type):
         vnum = self.shapes(type)
 
         if vnum == 3:
-            return self.TRIANGLE_AREA(type)
+            return self.TRI_AREA(type)
 
         elif vnum == 4:
-            return self.QUADR_AREA(type)
+            return self.QUAD_AREA(type)
 
     def __init__(self,file_name):
 
@@ -53,13 +78,14 @@ class Structure():
                         self.shapes.append(int(input[i+1]))
 
                 elif input[0] == 'vertices':
+
                     self.vertices = []
-                    shape_vertices = []
                     offset = 1
 
-                    for t in range(self.types):
+                    for n in range(self.types):
+                        shape_vertices = []
+                        for i in range(offset, self.shapes[n]*2 + offset):
 
-                        for i in range(offset, self.shapes[t]*2 + offset):
                             if list(input[i])[-1] == ',':
                                  nums = list((input[i]))[:-1]
                                  inp = nums[0]
@@ -68,8 +94,9 @@ class Structure():
                                  shape_vertices.append(float(inp))
                             else:
                                 shape_vertices.append(float(input[i]))
-                        offset += self.shapes[t]*2
-                        self.vertices.append(shape_vertices)
+
+                        offset += self.shapes[n]*2
+                        self.vertices.append(copy.deepcopy(shape_vertices))
 
 
 
@@ -78,8 +105,8 @@ class Structure():
 
         #area of each polygon
         self.type_areas = np.zeros(self.types)
-        for i in range(len(self.type_areas)):
-            self.type_areas[i] = self.AREA()
+        #for i in range(len(self.type_areas)):
+        #    self.type_areas[i] = self.AREA()
 
         #packing fraction
         self.packing_fraction = 0
@@ -103,6 +130,6 @@ class Shape():
     # Methods
     # Update the simulation environment
 
-    self.
 
 box = Structure("input.txt")
+print(box.vertices)
